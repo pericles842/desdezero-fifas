71e97a17d9b5357236816b99d9867c9d6fd592f6
Pericles <slouis482@gmail.com>
2025-04-25 19:40:03 -0400

    home y tarjetas estadisticas

--- src/app/web/web/web.component.ts
+++ src/app/web/web/web.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-web',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './web.component.scss'
 })
 export class WebComponent {
+  esCelular = window.innerWidth < 768;
 
+ 
+
+  @HostListener('window:resize')
+  onResize() {
+    this.esCelular = window.innerWidth < 768;
+  }
 }
