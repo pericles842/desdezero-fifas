963c95bac489d261d3d5eb810d20755358bf3036
Pericles <slouis482@gmail.com>
2025-05-02 00:05:57 -0400

    Se esta creando las bases del dasboard

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 import { LayoutComponent } from './layout.component';
 import { CommonModule } from '@angular/common';
+import { HeaderButtonsComponent } from './components/header-buttons/header-buttons.component';
+import { RifasFormComponent } from './components/rifas-form/rifas-form.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    RifasFormComponent,
+    HeaderButtonsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/admin/components/header-buttons/header-buttons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-buttons',
+  templateUrl: './header-buttons.component.html',
+  styleUrl: './header-buttons.component.css'
+})
+export class HeaderButtonsComponent {
+
+}
