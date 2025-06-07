868cba69eff5a849899e88055fbd4dabeacf3eb3
Pericles <slouis482@gmail.com>
2025-06-07 12:18:00 -0400

    Se parsea el monto en dolares

--- src/app/admin/components/card-statitics/card-statitics.component.ts
+++ src/app/admin/components/card-statitics/card-statitics.component.ts
@@ -17,13 +17,37 @@ export class CardStatiticsComponent {
 
   getTextSizeClass(text: string): string {
     const length = text.length;
-    
+
     if (length >= 20) {
       return 'text-xs';
-    }else {
+    } else {
       return 'text-4xl';
     }
 
+  }
+  parseValue(valor: string | number): string|number {
+    // Convertimos a string por si es un número
+    const valorStr = valor.toString();
+
+    // Paso 1: Verificar si contiene el símbolo $
+    const tieneDolar = valorStr.includes('$');
+
+    // Si no tiene el símbolo $, retornar el valor original sin cambios
+    if (!tieneDolar) {
+      return valor;
+    }
+
+    // Paso 2: Quitar el símbolo $
+    const valorSinDolar = valorStr.replace('$', '');
+
+    // Paso 3: Convertir a número
+    const numero = parseFloat(valorSinDolar);
+
+    // Paso 4: Redondear a 2 decimales
+    const numeroFormateado = numero.toFixed(2);
+
+    // Paso 5: Agregar el símbolo $ nuevamente
+    return `${numeroFormateado}$`;
   }
 
 }
